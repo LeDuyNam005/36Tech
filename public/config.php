c2cdc52b946ad91d07d420f1d21f6daa3c052287
Lynx <maihang122th@gmail.com>
2025-12-18 14:47:01 +0700

    upload from Dao-Loc-Branch
    this is made by me !

---
+++ public/config.php
@@ -0,0 +1,5 @@
+<?php
+// Định nghĩa đường dẫn gốc của dự án
+define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/public/');
+define('ASSETS', 'http://' . $_SERVER['HTTP_HOST'] . '/public/assets/');
+define('AUTH', 'http://' . $_SERVER['HTTP_HOST'] . '/app/');
